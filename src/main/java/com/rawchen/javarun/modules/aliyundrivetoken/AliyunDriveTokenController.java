fd42ca30ba87c57aee475f84fac96934f5ad8ba8
RawChen <2221999792@qq.com>
2022-12-21 23:48:44 +0800

    fix: aliyundrive token

--- src/main/java/com/rawchen/javarun/modules/aliyundrivetoken/AliyunDriveTokenController.java
+++ src/main/java/com/rawchen/javarun/modules/aliyundrivetoken/AliyunDriveTokenController.java
@@ -4,14 +4,14 @@ import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson.JSONObject;
-import com.rawchen.javarun.modules.onedrive.OneDriveService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 获取阿里云盘refreshToken
@@ -22,14 +22,11 @@ import javax.annotation.Resource;
 @Controller
 public class AliyunDriveTokenController {
 
-    @Resource
-    private OneDriveService oneDriveServiceImpl;
-
-    private String mobileLoginTokenApi = "https://passport.aliyundrive.com/newlogin/qrcode/generate.do?appName=aliyun_drive&isMobile=true";
-    private String querryApi = "https://passport.aliyundrive.com/newlogin/qrcode/query.do?appName=aliyun_drive&fromSite=52&_bx-v=2.0.31";
+    private final String mobileLoginTokenApi = "https://passport.aliyundrive.com/newlogin/qrcode/generate.do?appName=aliyun_drive&isMobile=true";
+    private final String querryApi = "https://passport.aliyundrive.com/newlogin/qrcode/query.do?appName=aliyun_drive&fromSite=52&_bx-v=2.0.31";
 
     @GetMapping("/alipan")
-    public String alipan(String code, Model model) {
+    public String alipan(Model model) {
         HttpRequest request = HttpUtil.createGet(mobileLoginTokenApi);
         HttpResponse response = request.execute();
         JSONObject jsonObject = JSONObject.parseObject(response.body());
@@ -39,26 +36,20 @@ public class AliyunDriveTokenController {
         String t = data.getString("t");
         String ck = data.getString("ck");
         model.addAttribute("codeContent", codeContent);
-        model.addAttribute("qrcode", "https://java.rawchen.com/qrcode/api?url=" + codeContent);
+        model.addAttribute("qrcode", "https://api.rawchen.com/api/qrcode?url=" + codeContent);
         model.addAttribute("t", t);
         model.addAttribute("ck", ck);
-        return "/aliyundrivetoken/index";
+        return "aliyundrivetoken/index";
     }
 
     @ResponseBody
     @PostMapping("/api/aliyundrivetoken/query")
-    public String query() {
-        HttpRequest request = HttpUtil.createPost(querryApi);
-        HttpResponse response = request.execute();
-        JSONObject jsonObject = JSONObject.parseObject(response.body());
-        JSONObject content = (JSONObject) jsonObject.get("content");
-        JSONObject data = (JSONObject) content.get("data");
-        String qrCodeStatus = data.getString("qrCodeStatus");
-        if ("CONFIRMED".equals(qrCodeStatus)) {
-            return data.getString("bizExt");
-        } else {
-            return "qrCodeStatus";
-        }
+    public JSONObject query(String t, String ck) {
+        Map<String, Object> param = new HashMap<>();
+        param.put("t", t);
+        param.put("ck", ck);
+        String post = HttpUtil.post(querryApi, param);
+        return JSONObject.parseObject(post);
     }
 
 }
