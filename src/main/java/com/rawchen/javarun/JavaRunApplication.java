f4aca446a077ca46a187b3d76692d7994abacfb7
RawChen <2221999792@qq.com>
2024-01-10 09:48:50 +0800

    fix: 警告信息

--- src/main/java/com/rawchen/javarun/JavaRunApplication.java
+++ src/main/java/com/rawchen/javarun/JavaRunApplication.java
@@ -1,7 +1,12 @@
 package com.rawchen.javarun;
 
+import org.apache.tomcat.util.http.LegacyCookieProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -9,5 +14,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class JavaRunApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaRunApplication.class, args);
+	}
+
+	@Bean
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
+		return tomcatServletWebServerFactory -> tomcatServletWebServerFactory.addContextCustomizers((TomcatContextCustomizer) context -> {
+			context.setCookieProcessor(new LegacyCookieProcessor());
+		});
 	}
 }
